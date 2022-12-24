3291b00c96f4f1d634bed3fcc62d23c33e45f538
JEONGKUNHO <dghj6739@gmail.com>
2022-12-24 20:59:30 +0900

    프로젝트 생성

---
+++ PyeonHae/PyeonHae/PyeonHaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyeonHaeApp.swift
+//  PyeonHae
+//
+//  Created by 정건호 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PyeonHaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
