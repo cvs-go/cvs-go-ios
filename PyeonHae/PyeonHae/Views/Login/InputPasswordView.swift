2eabfbfdda5145c6975fdcfed39ea77204ef95ca
JEONGKUNHO <dghj6739@gmail.com>
2022-12-27 23:59:40 +0900

    비밀번호 입력 뷰 추가

---
+++ PyeonHae/PyeonHae/Views/Login/InputPasswordView.swift
@@ -0,0 +1,31 @@
+//
+//  InputPasswordView.swift
+//  PyeonHae
+//
+//  Created by 정건호 on 2022/12/27.
+//
+
+import SwiftUI
+
+struct InputPasswordView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("비밀번호를\n입력해주세요.")
+                .font(.pretendard(.medium, 24))
+                .foregroundColor(.grayscale100)
+            Spacer().frame(height: 8)
+            Text("‘사용자 아이디@gmail.com’로 로그인합니다.")
+                .font(.pretendard(.regular, 14))
+                .foregroundColor(.grayscale85)
+                .padding(.leading, 2)
+            Spacer().frame(height: 31)
+            TextFieldWithTitle(
+                title: "비밀번호",
+                placeholder: "비밀번호를 입력해주세요.",
+                isSecure: true
+            )
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 23, leading: 20, bottom: 0, trailing: 20))
+    }
+}
--- PyeonHae/PyeonHae/Views/Login/LoginView.swift
+++ PyeonHae/PyeonHae/Views/Login/LoginView.swift
@@ -11,38 +11,43 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            TextFieldWithTitle(title: "아이디", placeholder: "이메일을 입력해주세요.", isSecure: false)
+        NavigationView {
+            VStack {
+                TextFieldWithTitle(
+                    title: "아이디",
+                    placeholder: "이메일을 입력해주세요.",
+                    isSecure: false
+                )
                 .padding(.horizontal, 20)
-            Spacer().frame(height: 10)
-            Button(action: {
-                print("다음 화면")
-            }) {
+                Spacer().frame(height: 10)
+                NavigationLink(destination: InputPasswordView()) {
                 Text("계속하기")
                     .font(.pretendard(.bold, 18))
                     .foregroundColor(.white)
+                    .frame(maxWidth: UIWindow().screen.bounds.width - 40, maxHeight: 50)
+                
+                    .background(Color.red100)
+                    .cornerRadius(10)
             }
-            .frame(maxWidth: UIWindow().screen.bounds.width - 40, maxHeight: 50)
-            .background(Color.red100)
-            .cornerRadius(10)
             Spacer().frame(height: 29)
-            Text("간편 로그인")
-                .font(.pretendard(.bold, 14))
-                .foregroundColor(.grayscale100)
-            Spacer().frame(height: 20)
-            HStack(spacing: 16) {
-                Image(name: .kakaoLogin)
-                    .onTapGesture {
-                        print("kakaoLogin")
-                    }
-                Image(name: .naverLogin)
-                    .onTapGesture {
-                        print("naverLogin")
-                    }
-                Image(name: .appleLogin)
-                    .onTapGesture {
-                        print("appleLogin")
-                    }
+                Text("간편 로그인")
+                    .font(.pretendard(.bold, 14))
+                    .foregroundColor(.grayscale100)
+                Spacer().frame(height: 20)
+                HStack(spacing: 16) {
+                    Image(name: .kakaoLogin)
+                        .onTapGesture {
+                            print("kakaoLogin")
+                        }
+                    Image(name: .naverLogin)
+                        .onTapGesture {
+                            print("naverLogin")
+                        }
+                    Image(name: .appleLogin)
+                        .onTapGesture {
+                            print("appleLogin")
+                        }
+                }
             }
         }
     }
