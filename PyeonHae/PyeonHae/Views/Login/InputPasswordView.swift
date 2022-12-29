b5d3cdf7d00ce2b2c35d658565b2525af1e4dc2e
JEONGKUNHO <dghj6739@gmail.com>
2022-12-30 00:03:48 +0900

    로그인, 비밀번호 입력 화면 키보드 위 버튼 추가

--- PyeonHae/PyeonHae/Views/Login/InputPasswordView.swift
+++ PyeonHae/PyeonHae/Views/Login/InputPasswordView.swift
@@ -8,24 +8,37 @@
 import SwiftUI
 
 struct InputPasswordView: View {
+    @FocusState private var isFocused
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("비밀번호를\n입력해주세요.")
-                .font(.pretendard(.medium, 24))
-                .foregroundColor(.grayscale100)
-            Spacer().frame(height: 8)
-            Text("‘사용자 아이디@gmail.com’로 로그인합니다.")
-                .font(.pretendard(.regular, 14))
-                .foregroundColor(.grayscale85)
-                .padding(.leading, 2)
-            Spacer().frame(height: 31)
-            TextFieldWithTitle(
-                title: "비밀번호",
-                placeholder: "비밀번호를 입력해주세요.",
-                isSecure: true
-            )
-            Spacer()
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("비밀번호를\n입력해주세요.")
+                        .font(.pretendard(.medium, 24))
+                        .foregroundColor(.grayscale100)
+                    Spacer().frame(height: 8)
+                    Text("‘사용자 아이디@gmail.com’로 로그인합니다.")
+                        .font(.pretendard(.regular, 14))
+                        .foregroundColor(.grayscale85)
+                        .padding(.leading, 2)
+                    Spacer().frame(height: 31)
+                    TextFieldWithTitle(
+                        title: "비밀번호",
+                        placeholder: "비밀번호를 입력해주세요.",
+                        isSecure: true
+                    )
+                    .focused($isFocused)
+                    Spacer()
+                }
+                .padding(EdgeInsets(top: 23, leading: 20, bottom: 0, trailing: 20))
+            }
         }
-        .padding(EdgeInsets(top: 23, leading: 20, bottom: 0, trailing: 20))
+        Text("로그인")
+            .font(.pretendard(.bold, 18))
+            .foregroundColor(.white)
+            .frame(width: UIWindow().screen.bounds.width - (isFocused ? 0 : 40), height: 50)
+            .background(Color.red100)
+            .cornerRadius(isFocused ? 0 : 10)
+        Spacer().frame(height: isFocused ? 0 : 52)
     }
 }
--- PyeonHae/PyeonHae/Views/Login/LoginView.swift
+++ PyeonHae/PyeonHae/Views/Login/LoginView.swift
@@ -7,47 +7,61 @@
 
 import SwiftUI
 
-// TODO: 추후 이미지 추가 예정, 키보드 위 버튼 추가
-
 struct LoginView: View {
+    @FocusState private var isFocused
     var body: some View {
         NavigationView {
             VStack {
-                TextFieldWithTitle(
-                    title: "아이디",
-                    placeholder: "이메일을 입력해주세요.",
-                    isSecure: false
-                )
-                .padding(.horizontal, 20)
-                Spacer().frame(height: 10)
-                NavigationLink(destination: InputPasswordView()) {
+                ScrollView {
+                    // TODO: 추후 이미지 추가 예정
+                    Text("대략적인 이미지 크기")
+                        .frame(width: UIWindow().screen.bounds.width - 40, height: 300)
+                    VStack {
+                        TextFieldWithTitle(
+                            title: "아이디",
+                            placeholder: "이메일을 입력해주세요.",
+                            isSecure: false
+                        )
+                        .focused($isFocused)
+                        .padding(.horizontal, 20)
+                        Spacer().frame(height: 10)
+                        if !isFocused {
+                            NavigationLink(destination: InputPasswordView()) {
+                                Text("계속하기")
+                                    .font(.pretendard(.bold, 18))
+                                    .foregroundColor(.white)
+                                    .frame(width: UIWindow().screen.bounds.width - 40, height: 50)
+                                    .background(Color.red100)
+                                    .cornerRadius(10)
+                            }
+                            Spacer().frame(height: 29)
+                            Text("간편 로그인")
+                                .font(.pretendard(.bold, 14))
+                                .foregroundColor(.grayscale100)
+                            Spacer().frame(height: 20)
+                            HStack(spacing: 16) {
+                                Image(name: .kakaoLogin)
+                                    .onTapGesture {
+                                        print("kakaoLogin")
+                                    }
+                                Image(name: .naverLogin)
+                                    .onTapGesture {
+                                        print("naverLogin")
+                                    }
+                                Image(name: .appleLogin)
+                                    .onTapGesture {
+                                        print("appleLogin")
+                                    }
+                            }
+                        }
+                    }
+                }
                 Text("계속하기")
                     .font(.pretendard(.bold, 18))
                     .foregroundColor(.white)
-                    .frame(maxWidth: UIWindow().screen.bounds.width - 40, maxHeight: 50)
-                
+                    .frame(width: UIWindow().screen.bounds.width, height: 50)
                     .background(Color.red100)
-                    .cornerRadius(10)
-            }
-            Spacer().frame(height: 29)
-                Text("간편 로그인")
-                    .font(.pretendard(.bold, 14))
-                    .foregroundColor(.grayscale100)
-                Spacer().frame(height: 20)
-                HStack(spacing: 16) {
-                    Image(name: .kakaoLogin)
-                        .onTapGesture {
-                            print("kakaoLogin")
-                        }
-                    Image(name: .naverLogin)
-                        .onTapGesture {
-                            print("naverLogin")
-                        }
-                    Image(name: .appleLogin)
-                        .onTapGesture {
-                            print("appleLogin")
-                        }
-                }
+                    .opacity(isFocused ? 1 : 0)
             }
         }
     }
