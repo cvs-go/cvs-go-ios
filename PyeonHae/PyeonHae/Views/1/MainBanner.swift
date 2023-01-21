118b87eaaae2d68ebcb38c7c9e41e3c4e2698530
JEONGKUNHO <dghj6739@gmail.com>
2023-01-21 19:24:59 +0900

    작업 분할을 위한 폴더, 파일 생성

---
+++ PyeonHae/PyeonHae/Views/1/MainBanner.swift
@@ -0,0 +1,20 @@
+//
+//  MainBanner.swift
+//  PyeonHae
+//
+//  Created by 정건호 on 2023/01/21.
+//
+
+import SwiftUI
+
+struct MainBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainBanner_Previews: PreviewProvider {
+    static var previews: some View {
+        MainBanner()
+    }
+}
