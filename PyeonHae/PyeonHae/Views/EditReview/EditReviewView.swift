1b3d1e7bfefaf5142c2bd0cd576a159b4866554a
JEONGKUNHO <dghj6739@gmail.com>
2023-02-18 16:46:46 +0900

    리뷰 작성 뷰 TopBar 추가

--- PyeonHae/PyeonHae/Views/EditReview/EditReviewView.swift
+++ PyeonHae/PyeonHae/Views/EditReview/EditReviewView.swift
@@ -8,21 +8,40 @@
 import SwiftUI
 
 struct EditReviewView: View {
+    @Binding var showWriteView: Bool
+    
     var body: some View {
         GeometryReader { geo in
             VStack(spacing: 0) {
-                    SelectProductView()
-                    Spacer().frame(height: 20)
-                    EditLetterView()
-                    ReviewPhotoView()
+                reviewTopBar
+                SelectProductView()
+                Rectangle()
+                    .frame(height: 14)
+                    .foregroundColor(Color.grayscale10)
+                EditLetterView()
+                ReviewPhotoView()
             }
-            .background(Color.grayscale10)
+            .background(Color.white)
         }
     }
-}
-
-struct EditReviewView_Previews: PreviewProvider {
-    static var previews: some View {
-        EditReviewView()
+    
+    var reviewTopBar: some View {
+        HStack(spacing: 0) {
+            Spacer().frame(width: 14)
+            Image(name: .close)
+                .onTapGesture {
+                    showWriteView = false
+                }
+            Spacer().frame(width: 9)
+            Text("리뷰 작성")
+                .font(.pretendard(.bold, 20))
+                .foregroundColor(.grayscale100)
+            Spacer()
+            Text("완료")
+                .font(.pretendard(.bold, 16))
+                .foregroundColor(.grayscale50)
+            Spacer().frame(width: 20)
+        }
+        .frame(height: 44)
     }
 }
