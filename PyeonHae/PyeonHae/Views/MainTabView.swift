b8f6410a97b3cb56621d6e7e00ef763dc2cb7de6
Juhwa-Lee1023 <juhwa.lee1023@gmail.com>
2023-02-21 21:07:17 +0900

    Feat[#20] : 검색결과창 아이템셀 구현

--- PyeonHae/PyeonHae/Views/MainTabView.swift
+++ PyeonHae/PyeonHae/Views/MainTabView.swift
@@ -31,7 +31,7 @@ struct MainTabView: View {
         case .review:
             ReviewHome()
         case .search:
-            Text("탐색")
+            SearchResultView()
         case .myPage:
             Text("내정보")
         }
