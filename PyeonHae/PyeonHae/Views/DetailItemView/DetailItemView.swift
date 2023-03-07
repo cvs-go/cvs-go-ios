bffaea40dba4868ceac7a33a936e40889fdba31c
Juhwa-Lee1023 <juhwa.lee1023@gmail.com>
2023-03-07 19:08:50 +0900

    Feat[#27] : scroll location detection

--- PyeonHae/PyeonHae/Views/DetailItemView/DetailItemView.swift
+++ PyeonHae/PyeonHae/Views/DetailItemView/DetailItemView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct DetailItemView: View {
     
+    @State private var isReviewButtonVisible = false
+    
     var body: some View {
         ZStack {
             VStack(alignment: .leading) {
                 DetailItemViewTopBar
                 ScrollView {
                     ItemDetailView()
+                        .background(
+                            GeometryReader { geometry -> Color in
+                                let maxY = geometry.frame(in: .global).midY
+                                if maxY <= 0 && !isReviewButtonVisible {
+                                    DispatchQueue.main.async {
+                                        isReviewButtonVisible = true
+                                    }
+                                } else if maxY > 0 && isReviewButtonVisible {
+                                    DispatchQueue.main.async {
+                                        isReviewButtonVisible = false
+                                    }
+                                }
+                                return Color.clear
+                            }
+                        )
                     Rectangle()
                         .frame(height: 14)
                         .foregroundColor(Color.grayscale10)
@@ -24,33 +41,48 @@ struct DetailItemView: View {
                         .foregroundColor(Color.white)
                 }
             }
-            VStack{
-                Spacer()
-                Button(action: {
-                
-                }){
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(.red100)
-                        Text("리뷰 작성")
-                            .font(.pretendard(.bold, 18))
-                            .foregroundColor(.white)
+                VStack{
+                    Spacer()
+                    Button(action: {
+                    
+                    }){
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.red100)
+                            Text("리뷰 작성")
+                                .font(.pretendard(.bold, 18))
+                                .foregroundColor(.white)
+                        }
+                        .frame(height: 50)
+                        .padding(.horizontal, 20)
                     }
-                    .frame(height: 50)
-                    .padding(.horizontal, 20)
                 }
-            }
         }
     }
-    
     var DetailItemViewTopBar: some View {
         HStack(spacing: 0) {
             Spacer().frame(width: 14)
             Image(name: .arrowLeft)
+            Spacer().frame(width: 18)
+            if isReviewButtonVisible {
+                Text("백두마운틴바")
+                    .font(.pretendard(.bold, 18))
+                    .foregroundColor(.black)
+            }
+            Spacer()
         }
         .frame(height: 44)
     }
 }
+
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
 
 struct DetailItemView_Previews: PreviewProvider {
     static var previews: some View {
