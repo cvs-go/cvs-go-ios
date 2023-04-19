3d5b3206b2c49756cc05a99039718457e4da0086
JEONGKUNHO <dghj6739@gmail.com>
2023-04-19 22:54:28 +0900

    feat: 검색창 포커스 UI 구현

--- PyeonHae/PyeonHae/ReusableUI/SearchBar.swift
+++ PyeonHae/PyeonHae/ReusableUI/SearchBar.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct SearchBar: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     @Binding var text: String
+    let searchBarType: searchBarType
+    @FocusState private var isFocused
+    @State private var startSearch = false
+    
     var body: some View {
-        VStack(spacing: 8) {
-            HStack {
-                // 백버튼 없으면 disable 시키기
+        VStack(spacing: 9) {
+            HStack(spacing: 0) {
+                if searchBarType == .editable {
+                    Image(name: .arrowLeft)
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .onTapGesture {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                }
                 TextField(
                     String(),
                     text: $text,
@@ -20,15 +34,51 @@ struct SearchBar: View {
                         .font(.pretendard(.regular, 14))
                         .foregroundColor(.grayscale50)
                 )
+                .focused($isFocused)
                 Image(name: .searchIcon)
                     .resizable()
                     .frame(width: 24, height: 24)
                     .padding(.vertical, 2)
             }
-            .padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 18))
-            
-            Color.grayscale30
-                .frame(width: UIWindow().screen.bounds.width, height: 1)
+            .padding(EdgeInsets(
+                top: 8,
+                leading: searchBarType == .editable ? 14 : 20,
+                bottom: 0,
+                trailing: 18)
+            )
         }
+        .onAppear {
+            if searchBarType == .uneditable {
+                self.isFocused = false
+                self.text = String()
+            } else {
+                self.isFocused = true
+            }
+        }
+        .onTapGesture {
+            if searchBarType == .uneditable {
+                startSearch = true
+            }
+        }
+        .overlay(
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(isFocused ? Color.red100 : Color.grayscale30)
+                .offset(y: 8)
+            , alignment: .bottom
+        )
+        
+        NavigationLink(
+            destination: SearchStartView(text: $text).navigationBarHidden(true),
+            isActive: $startSearch)
+        {
+            EmptyView()
+        }
+        .padding(.bottom, 8)
     }
+}
+
+enum searchBarType {
+    case editable
+    case uneditable
 }
