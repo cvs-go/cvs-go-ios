450b4c897565f5d246137e93147c124a85d7cf02
Juhwa-Lee1023 <juhwa.lee1023@gmail.com>
2023-05-21 20:01:49 +0900

    Feat : 리뷰수정페이지 키보드 버튼 추가

--- PyeonHae/PyeonHae/Views/Review/EditReview/EditReviewView.swift
+++ PyeonHae/PyeonHae/Views/Review/EditReview/EditReviewView.swift
@@ -12,24 +12,60 @@ class ImageSelection: ObservableObject {
 }
 
 struct EditReviewView: View {
+    @StateObject private var keyboardResponder = KeyboardResponder()
     @StateObject var imageSelection = ImageSelection()
     @Binding var showWriteView: Bool
     @State private var showToast = false
+    @State private var showImagePicker = false
     @State private var showSearchProductView = false
     
     var body: some View {
         GeometryReader { geo in
-            VStack(spacing: 0) {
-                reviewTopBar
-                SelectProductView(showSearchProductView: $showSearchProductView)
-                Rectangle()
-                    .frame(height: 14)
-                    .foregroundColor(Color.grayscale10)
-                EditLetterView()
-                ReviewPhotoView(imageSelection: imageSelection, showToast: $showToast)
+            ZStack {
+                Color.white
+                VStack(spacing: 0) {
+                    reviewTopBar
+                    SelectProductView(showSearchProductView: $showSearchProductView)
+                    Rectangle()
+                        .frame(height: 14)
+                        .foregroundColor(Color.grayscale10)
+                    EditLetterView()
+                    if(keyboardResponder.currentHeight == 0) {
+                        ReviewPhotoView(imageSelection: imageSelection, showToast: $showToast)
+                    }
+                }
+                if(keyboardResponder.currentHeight > 0) {
+                    VStack(spacing: 0) {
+                        Spacer()
+                        Divider()
+                        Button(action: {
+                            if imageSelection.images.count < 3 {
+                                showImagePicker = true
+                            } else {
+                                showToast = true
+                            }
+                        }) {
+                            HStack(spacing: 10) {
+                                Image(name: .plusButton)
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("사진 추가 (\(imageSelection.images.count)/3)")
+                                    .font(.pretendard(.medium, 16))
+                                    .foregroundColor(.grayscale70)
+                                Spacer()
+                            }
+                            .padding(.horizontal, 22)
+                            .padding(.vertical, 12)
+                            .frame(height: 44)
+                            .background(Color.grayscale20)
+                        }
+                    }
+                }
             }
-            .background(Color.white)
             .toast(message: "사진은 최대 3장까지 추가할 수 있습니다.", isShowing: $showToast, config: .init())
+        }
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(images: $imageSelection.images)
         }
         .blur(radius: showSearchProductView ? 2 : 0)
         .bottomSheet(isPresented: $showSearchProductView) {
