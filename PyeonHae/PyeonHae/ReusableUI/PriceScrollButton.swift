c674982f86afe5f508545fdd829f1e8729bbc1b2
Juhwa-Lee1023 <juhwa.lee1023@gmail.com>
2023-05-28 19:35:31 +0900

    Bug : priceScrollButton Bug Fix

--- PyeonHae/PyeonHae/ReusableUI/PriceScrollButton.swift
+++ PyeonHae/PyeonHae/ReusableUI/PriceScrollButton.swift
@@ -16,86 +16,95 @@ struct PriceScrollButton: View {
     let trackWidth: CGFloat = 335
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 6) {
-            HStack {
-                Text("가격")
-                    .font(.pretendard(.bold, 12))
-                    .foregroundColor(Color.grayscale85)
-                Spacer()
-                if (minPrice == 0 && maxPrice == 1) {
-                    Text("전체")
-                        .font(.pretendard(.regular, 12))
-                        .foregroundColor(Color.grayscale70)
-                        .padding(.trailing, 20)
-                } else {
-                    Text("\(Int(minPrice * 15000))원~\(Int(maxPrice * 15000))원")
-                        .font(.pretendard(.regular, 12))
-                        .foregroundColor(Color.grayscale70)
-                        .padding(.trailing, 20)
+        GeometryReader { geometry in
+            let trackWidth = geometry.size.width
+            VStack(alignment: .leading, spacing: 6) {
+                HStack {
+                    Text("가격")
+                        .font(.pretendard(.bold, 12))
+                        .foregroundColor(Color.grayscale85)
+                    Spacer()
+                    if (minPrice == 0 && maxPrice == 1) {
+                        Text("전체")
+                            .font(.pretendard(.regular, 12))
+                            .foregroundColor(Color.grayscale70)
+                    } else {
+                        Text("\(Int(minPrice * 15000))원~\(Int(maxPrice * 15000))원")
+                            .font(.pretendard(.regular, 12))
+                            .foregroundColor(Color.grayscale70)
+                    }
                 }
-            }
-            ZStack {
-                HStack(spacing: 0) {
-                    Rectangle()
-                        .foregroundColor(Color.grayscale30)
-                        .frame(width: trackWidth * minPrice, height: 8)
-                    Rectangle()
-                        .foregroundColor(Color.grayscale100)
-                        .frame(width: trackWidth * (maxPrice - minPrice), height: 8)
-                    Rectangle()
-                        .foregroundColor(Color.grayscale30)
-                        .frame(width: trackWidth * (1 - maxPrice), height: 8)
+                ZStack {
+                    HStack(spacing: 0) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(minPrice > 0 ? Color.grayscale30 : Color.grayscale100)
+                            .frame(width: trackWidth * minPrice + 16, height: 8)
+                        Rectangle()
+                            .foregroundColor(Color.grayscale100)
+                            .frame(width: trackWidth * (maxPrice - minPrice) - 32, height: 8)
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(maxPrice < 1 ? Color.grayscale30 : Color.grayscale100)
+                            .frame(width: trackWidth * (1 - maxPrice) + 16, height: 8)
+                    }
+
+                    HStack(spacing: 0) {
+                        Image(name: .PriceScrollButton)
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .padding(.leading, trackWidth * minPrice)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { value in
+                                        if priceTemp == 0 {
+                                            lastEditPrice = minPrice
+                                        }
+                                        let addMinPrice = max(0, (minPrice * trackWidth + value.translation.width) / trackWidth - priceTemp)
+                                        if addMinPrice < maxPrice - (64 / trackWidth) {
+                                            minPrice = addMinPrice
+                                        } else {
+                                            minPrice = maxPrice - (64 / trackWidth)
+                                        }
+                                        priceTemp = minPrice - lastEditPrice
+                                    }
+                                    .onEnded { value in
+                                        priceTemp = 0
+                                    }
+                            )
+                        Spacer().frame(width: max(trackWidth * (maxPrice - minPrice) - 64, 0))
+                        Image(name: .PriceScrollButton)
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { value in
+                                        let newMaxPrice = min(1, (maxPrice * trackWidth + value.translation.width) / trackWidth)
+                                        if newMaxPrice > minPrice + (64 / trackWidth) {
+                                            maxPrice = newMaxPrice
+                                        } else {
+                                            maxPrice = minPrice + (64 / trackWidth)
+                                        }
+                                    }
+                            )
+                        Spacer().frame(width: max(trackWidth * (1 - maxPrice), 0))
+                    }
                 }
                 HStack {
-                    Image(name: .PriceScrollButton)
-                        .resizable()
-                        .frame(width: 32, height: 32)
-                        .padding(.leading, trackWidth * minPrice)
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    if priceTemp == 0 {
-                                        lastEditPrice = minPrice
-                                    }
-                                    let addMinPrice = max(0, max(0, (minPrice * trackWidth + value.translation.width) / trackWidth) - priceTemp)
-                                    if addMinPrice < maxPrice {
-                                        minPrice = addMinPrice
-                                    }
-                                    priceTemp = minPrice - lastEditPrice
-                                }
-                                .onEnded { value in
-                                    priceTemp = 0
-                                }
-                        )
-                    Spacer().frame(width: max(trackWidth * (maxPrice - minPrice) - 32, 0))
-                    Image(name: .PriceScrollButton)
-                        .resizable()
-                        .frame(width: 32, height: 32)
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    let newMaxPrice = min(1, (maxPrice * trackWidth + value.translation.width) / trackWidth)
-                                    if newMaxPrice > minPrice {
-                                        maxPrice = newMaxPrice
-                                    }
-                                }
-                        )
+                    Text("최소")
+                        .font(.pretendard(.regular, 12))
+                        .foregroundColor(Color.grayscale70)
                     Spacer()
+                    Text("5천")
+                        .font(.pretendard(.regular, 12))
+                        .foregroundColor(Color.grayscale70)
+                    Spacer()
+                    Text("1만")
+                        .font(.pretendard(.regular, 12))
+                        .foregroundColor(Color.grayscale70)
+                    Spacer()
+                    Text("최대")
+                        .font(.pretendard(.regular, 12))
+                        .foregroundColor(Color.grayscale70)
                 }
-            }
-            HStack(spacing: 87) {
-                Text("최소")
-                    .font(.pretendard(.regular, 12))
-                    .foregroundColor(Color.grayscale70)
-                Text("5천")
-                    .font(.pretendard(.regular, 12))
-                    .foregroundColor(Color.grayscale70)
-                Text("1만")
-                    .font(.pretendard(.regular, 12))
-                    .foregroundColor(Color.grayscale70)
-                Text("최대")
-                    .font(.pretendard(.regular, 12))
-                    .foregroundColor(Color.grayscale70)
             }
         }
     }
