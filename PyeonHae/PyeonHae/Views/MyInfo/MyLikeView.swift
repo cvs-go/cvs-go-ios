1a023efec43dbf886b1fa2336cc2cccd7a7bdf5f
JEONGKUNHO <dghj6739@gmail.com>
2024-01-12 11:25:07 +0900

    feat: 내정보 좋아요 탭 정렬 기능 구현

--- PyeonHae/PyeonHae/Views/MyInfo/MyLikeView.swift
+++ PyeonHae/PyeonHae/Views/MyInfo/MyLikeView.swift
@@ -11,62 +11,66 @@ struct MyLikeView: View {
     @ObservedObject var myInfoViewModel: MyInfoViewModel
     @Binding var selectedProduct: Product?
     @Binding var showProductDetail: Bool
+    @State private var sortClicked = false
     
     var body: some View {
         VStack(spacing: 0) {
             Spacer().frame(height: 12)
-            HStack {
-                HStack(spacing: 2) {
+            ZStack(alignment: .top) {
+                HStack(alignment: .top, spacing: 2) {
                     Text("좋아요한 제품")
                         .font(.pretendard(.regular, 12))
                         .foregroundColor(.grayscale85)
+                        .padding(.top, 7)
                     Text("\(myInfoViewModel.myLikeData?.totalElements ?? 0)개")
                         .font(.pretendard(.bold, 12))
                         .foregroundColor(.grayscale85)
+                        .padding(.top, 7)
                     Spacer()
-                    HStack(spacing: 6) {
-                        Text("최신순")
-                            .font(.pretendard(.regular, 12))
-                            .foregroundColor(.grayscale85)
-                        Image(name: .invertedTriangle)
-                    }
-                    .frame(width: 64.5, height: 26)
-                    .background(Color.grayscale10)
-                    .cornerRadius(10)
+                    SortSelectView(
+                        sortType: .product,
+                        sortBy: $myInfoViewModel.likeSortBy,
+                        sortClicked: $sortClicked
+                    )
                 }
-            }
-            .frame(height: 40)
-        }
-        .padding(.horizontal,20)
-        if let myLikeData = myInfoViewModel.myLikeData {
-            ScrollView {
-                ForEach(myLikeData.content, id: \.self) { product in
-                    VStack {
-                        SearchResultItemView(
-                            selectedProduct: $selectedProduct,
-                            isHeartMark: product.isLiked,
-                            isBookMark: product.isBookmarked,
-                            product: product,
-                            productViewType: .search,
-                            likeAction: {
-                                myInfoViewModel.requestProductLike(productID: product.productId)
-                            },
-                            unlikeAction: {
-                                myInfoViewModel.requestProductUnlike(productID: product.productId)
-                                myInfoViewModel.myLikeData?.content.removeAll(where: { $0.productId == product.productId })
-                                myInfoViewModel.myLikeData?.totalElements -= 1
-                            },
-                            bookmarkAction: {
-                                myInfoViewModel.requestProductBookmark(productID: product.productId)
-                            },
-                            unBookmarkAction: {
-                                myInfoViewModel.requestProductUnBookmark(productID: product.productId)
+                .padding(.horizontal,20)
+                .zIndex(1)
+                if let myLikeData = myInfoViewModel.myLikeData {
+                    ScrollView {
+                        ForEach(myLikeData.content, id: \.self) { product in
+                            VStack {
+                                SearchResultItemView(
+                                    selectedProduct: $selectedProduct,
+                                    isHeartMark: product.isLiked,
+                                    isBookMark: product.isBookmarked,
+                                    product: product,
+                                    productViewType: .search,
+                                    likeAction: {
+                                        myInfoViewModel.requestProductLike(productID: product.productId)
+                                    },
+                                    unlikeAction: {
+                                        myInfoViewModel.requestProductUnlike(productID: product.productId)
+                                        myInfoViewModel.myLikeData?.content.removeAll(where: { $0.productId == product.productId })
+                                        myInfoViewModel.myLikeData?.totalElements -= 1
+                                    },
+                                    bookmarkAction: {
+                                        myInfoViewModel.requestProductBookmark(productID: product.productId)
+                                    },
+                                    unBookmarkAction: {
+                                        myInfoViewModel.requestProductUnBookmark(productID: product.productId)
+                                    }
+                                )
                             }
-                        )
+                            .padding(.vertical, 10)
+                        }
                     }
-                    .padding(.vertical, 10)
+                    .offset(y: 50)
+                    .padding(.bottom, 50)
                 }
             }
+        }
+        .onChange(of: sortClicked) { _ in
+            myInfoViewModel.requestMyLikeList()
         }
     }
 }
